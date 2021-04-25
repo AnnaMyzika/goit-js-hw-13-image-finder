0f5629dde2f23204ef28ff1bd47b90f1dc953614
Anna Myzika <myzikaanya@gmail.com>
2021-04-25 23:43:16 +0300

    добавлена кнопка и скролл

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import apiServise from './js/apiService';
 import imgCard from './templates/imgCard.hbs';
 import * as basicLightbox from 'basiclightbox';
 import './css/basicLightBox.css';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   input: document.querySelector('input'),
   imgList: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load'),
 };
 
 const params = {
@@ -18,11 +22,21 @@ const params = {
 refs.searchForm.addEventListener('submit', getPictures);
 function getPictures(e) {
   e.preventDefault();
-  params.query = e.target.children[0].value;
+  refs.imgList.innerHTML = '';
+  params.query = e.target.children[0].value.trim();
+  params.page = 1;
+  // params.query = query;
   apiServise(params.query, params.page).then(({ hits }) => {
+    if (params.query === '') {
+      error({
+        text: 'Enter something',
+        delay: 2000,
+      });
+      return;
+    }
+    refs.loadMoreBtn.classList.add('is-open');
     refs.imgList.insertAdjacentHTML('beforeend', imgCard(hits));
   });
-  console.log(e.target.children[0].value);
 }
 
 refs.imgList.addEventListener('click', e => {
@@ -32,3 +46,13 @@ refs.imgList.addEventListener('click', e => {
       .show();
   }
 });
+
+refs.loadMoreBtn.addEventListener('click', loadMoreImage);
+function loadMoreImage() {
+  apiServise(params.query, params.page).then(({ hits }) => {
+    params.page += 1;
+    refs.imgList.insertAdjacentHTML('beforeend', imgCard(hits));
+    const totalScrollHeight = refs.imgList.clientHeight;
+    window.scrollTo({ top: totalScrollHeight, behavior: 'smooth' });
+  });
+}
